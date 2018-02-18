c0d96b80b8bd7edbc8bd17bd3233341bab1b5711
Ielool <xlyyvonne@hotmail.com>
2018-02-19 00:40:32 +0800

    debug

--- dev/666_log/src/cn/edu/ruc/xowa/log/graph/GraphBuilder.java
+++ dev/666_log/src/cn/edu/ruc/xowa/log/graph/GraphBuilder.java
@@ -2,9 +2,7 @@ package cn.edu.ruc.xowa.log.graph;
 
 import cn.edu.ruc.xowa.log.page.Page;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GraphBuilder
@@ -18,14 +16,16 @@ public class GraphBuilder
 
     private String userName = null;
     private String sessionId = null;
-    private List<GraphNode> rootNodes = null;
-    private Map<String, GraphNode> pointerNodes = null;
-    private Map<String, GraphNode> allNodes = null;
+    private GraphNode pageNode;
+    private GraphNode pointerNode;
+    private GraphNode rootNode;
+    private Map<String, GraphNode> allNodes;
 
     private GraphBuilder()
     {
-        this.rootNodes = new ArrayList<>();
-        this.pointerNodes = new HashMap<>();
+        this.pageNode = new GraphNode();
+        this.pointerNode = new GraphNode();
+        this.rootNode = new GraphNode();
         this.allNodes = new HashMap<>();
     }
 
@@ -43,30 +43,141 @@ public class GraphBuilder
 
     public void end ()
     {
+        this.pageNode = null;
+        this.pointerNode = null;
+        this.rootNode = null;
+        this.allNodes = null;
         System.out.println("end session...");
     }
 
     public void clear ()
     {
+        this.pageNode = null;
+        this.pointerNode = null;
+        this.rootNode = null;
+        this.allNodes = null;
         System.out.println("give up session, clear...");
     }
 
     public void goTo (Page page)
     {
-        System.out.println("go to:");
-        System.out.println("url:  " + page.getUrl());
-        System.out.println("prev: " + page.getPrevious());
-        System.out.println("root: " + page.getRoot());
-        System.out.println("#link:" + page.getLinks().size());
+        //System.out.println("page: "+page.getUrl().getKeyWord()+" "+page.getUrl());
+        try
+        {
+            pageNode.setName(page.getUrl().getKeyWord());
+            pageNode.setLinks(page.getLinks());
+            System.out.println("pageNode: "+ pageNode.getName());
+            //if pointerNode 为空
+            if(pointerNode.getName() == null)
+            {
+                System.out.println("case1");
+
+                rootNode.setName(page.getUrl().getKeyWord());
+                rootNode.setLinks(page.getLinks());
+                System.out.println("rootNode: "+ rootNode.getName());
+
+                pointerNode.setName(page.getUrl().getKeyWord());
+                pointerNode.setLinks(page.getLinks());
+                System.out.println("pointerNode: "+ pointerNode.getName());
+
+                allNodes.put(pageNode.getName(),pageNode);
+            }else
+            {
+                System.out.println("case2");
+                System.out.println("pointerNode: "+pointerNode.getName());
+                //rootNode
+                if(rootNode.getName() != null)
+                {
+                    // if rootNodes中存在page's root
+                    System.out.println("case2-1");
+                    if(rootNode.getName().equals(page.getRoot().getKeyWord())){
+                        //if pageNode存在于allNodes中
+                        System.out.println("case2-1-1");
+                        if(allNodes.containsKey(page.getUrl().getKeyWord()))
+                        {
+                            System.out.println("case2-1-1-1");
+                            pointerNode = allNodes.get(pageNode.getName());
+                        }else
+                        {
+                            System.out.println("case2-1-1-2");
+                            System.out.println("current pageNode: "+pageNode.getName());
+                            System.out.println("current pointerNode: "+pointerNode.getName());
+                            pointerNode.addChildren(pageNode);
+                            System.out.println("pointerNode: "+pointerNode.getName()+" children: "+ pointerNode.getChildren().keySet());
+                            pageNode.addParents(pointerNode);
+                            System.out.println("pageNode: "+ pageNode.getName()+" parents: " + pageNode.getParents().keySet());
+
+                            allNodes.put(pageNode.getName(),pageNode);
+                            pageNode = pointerNode;
+                            pointerNode = allNodes.get(pageNode.getName());
+                            System.out.println("the new pointerNode: "+pointerNode.getName());
+                        }
+
+                    }
+                    //：NO (OPEN A NEW TAB)
+                    else
+                    {
+                        //if page.url 在 pointerNode.links 中
+                        System.out.println("case2-1-2");
+                        //System.out.println("pointerNode.getName(): "+pointerNode.getName());
+                        //System.out.println("pointerNode.getLinks(): "+pointerNode.getLinks());
+                        if(pointerNode.getLinks().contains(page.getUrl()))
+                        {
+                            System.out.println("case2-1-2-1");
+                            allNodes.put(pageNode.getName(), pageNode);
+                            pointerNode.addChildren(pageNode);
+                            pageNode.addParents(pointerNode);
+                            pointerNode = allNodes.get(pageNode.getName());
+                        }
+                        //多个tab时，从非pointerNode(此前load的node)中load新页面
+                        else
+                        {
+                            //if
+                            System.out.println("case2-1-2-2");
+                            if(allNodes.containsKey(page.getRoot().getKeyWord()))
+                            {
+                                System.out.println("case2-1-2-2-1");
+                                allNodes.put(pageNode.getName(), pageNode);
+                                pointerNode = allNodes.get(page.getRoot().getKeyWord());
+                                pointerNode.addChildren(pageNode);
+                                pageNode.addParents(pointerNode);
+
+                                pointerNode = allNodes.get(pageNode.getName());
+                            }
+                            else
+                            {
+                                //......
+                                System.out.println("case2-1-2-2-2");
+                            }
+                        }
+                    }
+                }else
+                {
+                    System.out.println("case2-2");
+                    rootNode = pageNode;
+                }
+            }
+
+            //pointerNode.put(page.getUrl().getKeyWord(), );
+            System.out.println("allNodes: "+allNodes.keySet());
+            System.out.println("-----------------------------------------");
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        //System.out.println("go to:");
+        //System.out.println("url:  " + page.getUrl());
+        //System.out.println("prev: " + page.getPrevious());
+        //System.out.println("root: " + page.getRoot());
+        //System.out.println("#link:" + page.getLinks().size());
     }
 
     public void popupTo (Page page)
     {
-        System.out.println("popup to:");
-        System.out.println("url:  " + page.getUrl());
-        System.out.println("prev: " + page.getPrevious());
-        System.out.println("root: " + page.getRoot());
-        System.out.println("#link:" + page.getLinks().size());
+        //System.out.println("popup to:");
+        //System.out.println("url:  " + page.getUrl());
+        //System.out.println("prev: " + page.getPrevious());
+        //System.out.println("root: " + page.getRoot());
+        //System.out.println("#link:" + page.getLinks().size());
     }
 
     public void goBackward (Page from, Page to)
--- dev/666_log/src/cn/edu/ruc/xowa/log/graph/GraphNode.java
+++ dev/666_log/src/cn/edu/ruc/xowa/log/graph/GraphNode.java
@@ -2,13 +2,60 @@ package cn.edu.ruc.xowa.log.graph;
 
 import cn.edu.ruc.xowa.log.page.Url;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class GraphNode
 {
     private String name;
-    private List<Url> links;
-    private Map<String, GraphNode> parents;
-    private Map<String, GraphNode> children;
+    private Set<Url> links;
+    private Map<String, GraphNode> parents = new HashMap<>();
+    private Map<String, GraphNode> children = new HashMap<>();
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setLinks(Set<Url> links)
+    {
+        this.links = links;
+    }
+
+    public void addParents(GraphNode parent)
+    {
+        if(!this.getParents().containsKey(parent.getName()))
+        {
+            this.parents.put(parent.getName(), parent);
+        }
+    }
+
+    public void addChildren(GraphNode child)
+    {
+        if (!this.getChildren().containsKey(child.getName()))
+        {
+            this.children.put(child.getName(),child);
+        }
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Set<Url> getLinks()
+    {
+        return links;
+    }
+
+    public Map<String, GraphNode> getParents()
+    {
+        return parents;
+    }
+
+    public Map<String, GraphNode> getChildren()
+    {
+        return children;
+    }
 }
